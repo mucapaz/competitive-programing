c23849f41f53179b9eff975fd4f6eb4205e5d450
pazspm <pazspm@users.noreply.github.com>
2015-04-12 14:08:19 -0300

    Update eulerianPathAndCircuit.cpp

--- eulerianPathAndCircuit.cpp
+++ eulerianPathAndCircuit.cpp
@@ -2,5 +2,5 @@
 // Eulerian path and circuit - - spm lib
 /*
 In graph theory, a Eulerian trail (or Eulerian path) is a trail in a graph which visits every edge exactly once.
-Similarly, an Eulerian circuit or Eulerian cycle is an Eulerian trail which starts and ends on the same vertex.   
+Similarly, an Eulerian circuit or Eulerian cycle or Eulerian tour is an Eulerian trail which starts and ends on the same vertex.   
 */
