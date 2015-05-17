515a54a38f888af79aa35db7def8b7aa6088e2a7
pazspm <pazspm@users.noreply.github.com>
2015-05-17 16:17:47 -0300

    Create geometry.cpp

---
+++ geometry.cpp
@@ -0,0 +1,13 @@
+struct line { double a, b, c; };          // a way to represent a line
+
+// the answer is stored in the third parameter (pass by reference)
+void pointsToLine(point p1, point p2, line &l) {
+  if (fabs(p1.x - p2.x) < EPS) {              // vertical line is fine
+    l.a = 1.0;   l.b = 0.0;   l.c = -p1.x;           // default values
+  } else {
+    l.a = -(double)(p1.y - p2.y) / (p1.x - p2.x);
+    l.b = 1.0;              // IMPORTANT: we fix the value of b to 1.0
+    l.c = -(double)(l.a * p1.x) - p1.y;
+} }
+
+/////////////////////////////////////////////////////////////
